94aeb550df1a2030136ce95519655550f629af7e
smeerws <hello@smeerws.at>
2019-05-20 12:58:24 +0200

    wb build up MainScene

--- Assets/MyGame/Scripts/SceneLoader.cs
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour {
+public class SceneLoader : MonoBehaviour
+{
+    public static string MAINSCENE = "MainScene";
+    public static string GAMEOVER = "GameOverScene";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(MAINSCENE);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene(GAMEOVER);
+    }
 }
